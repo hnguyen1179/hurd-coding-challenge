e3581bef42cbb24bb2e5d5cbe6e3afe1526d2720
Duke <hnguyen1179@berkeley.edu>
2024-02-29 19:43:52 -0800

    Added form submission alert for incomplete form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,22 @@ export default function Login() {
     const form = event.currentTarget;
     const formData = new FormData(form);
 
-    alert(
-      `Submitted form with email as ${formData.get("email")} and password as ${formData.get("password")}`,
-    );
+    const email = formData.get("email");
+    const pw = formData.get("password");
+
+    if (!email || !pw) {
+      alert("Form incomplete!");
+      event.preventDefault(); // Prevents form from submittion--resetting the form
+    } else {
+      alert(
+        `Submitted form with email as ${formData.get("email")} and password as ${formData.get("password")}`,
+      );
+    }
   };
 
   return (
     <main className="flex h-full items-center justify-center bg-gradient-linear sm:p-24">
-      <div className="flex w-full grow flex-col gap-4 rounded-lg bg-black bg-opacity-85 p-8 h-[30rem] sm:max-w-[30rem] md:p-12">
+      <div className="flex h-[30rem] w-full grow flex-col gap-4 rounded-lg bg-black bg-opacity-85 p-8 sm:max-w-[30rem] md:p-12">
         <div className="text-hurd-purple-light my-4 text-5xl font-semibold">
           hurd
         </div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,11 @@
 const Header = () => {
   return (
-    <header className="flex bg-hurd-purple bg-opacity-95 px-8 py-4">
-      <a className="text-xl font-semibold text-stone-400" href="/">
+    <header className="flex bg-hurd-purple bg-opacity-95 px-8 py-2">
+      {/* Trick to align the margins with form in small windows */}
+      <a
+        className="relative -left-4 rounded-lg px-4 py-2 text-xl font-semibold text-stone-400 transition-colors hover:bg-purple-200 hover:bg-opacity-30"
+        href="/"
+      >
         hurd
       </a>
     </header>
