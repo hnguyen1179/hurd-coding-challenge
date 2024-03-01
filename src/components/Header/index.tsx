f3836ab558901b95116908f6f777e684ef3d9a37
Duke <hnguyen1179@berkeley.edu>
2024-02-29 17:58:02 -0800

    Added header component + login bg

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header className="bg-hurd-purple p-2 flex">
+			<a className="text-stone-400" href="/">
+				hurd
+			</a>
+		</header>
+	);
+};
+
+export default Header;
